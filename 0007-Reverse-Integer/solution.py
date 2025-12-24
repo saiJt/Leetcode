60adf9dbb2178c75f38d45602b5446cd3fb1b741
poeun <95955083+saiJt@users.noreply.github.com>
2025-12-24 00:35:38 -0800

    Implement reverse integer function in solution.py

---
+++ 0007-Reverse-Integer/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        int_max = 2**31 -1
+        int_min = -2**31
+
+        result = 0
+        digit = 0
+        neg = 0
+
+        if x < 0:
+            neg = 1
+            x = abs(x)
+        
+        while x != 0:
+            digit = x % 10
+            x //= 10
+
+            # overflow check
+            if result > int_max // 10 or (result == int_min // 10 and digit > 7):
+                return 0
+                
+            result = result * 10 + digit
+
+        if neg:
+            result = -result
+
+        return result
+

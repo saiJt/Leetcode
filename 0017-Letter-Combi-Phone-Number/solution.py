7449f3511db217d6e4867be0543d04d9a7dcdeb6
poeun <95955083+saiJt@users.noreply.github.com>
2025-12-30 19:55:28 -0800

    Implement letter combinations for phone numbers

---
+++ 0017-Letter-Combi-Phone-Number/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        letter = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        result = []
+
+        def dfs(index: int, path: str):
+            if index == len(digits):
+                result.append(path)
+                return
+
+            for ch in letter[digits[index]]:
+                dfs(index + 1, path + ch)
+
+        dfs(0, "")
+        return result

37842954d3ee88ca211f120c26e52cd10de492fc
poeun <95955083+saiJt@users.noreply.github.com>
2026-01-04 22:46:23 -0800

    Create solution.py

---
+++ 0028-Find-the-index-of-the-First-Occurrence-in-a-string/solution.py
@@ -0,0 +1,40 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+
+        i = 1
+        length = 0
+        lps = [0] * len(needle)
+
+        while i < len(needle):
+            if needle[i] == needle[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length > 0:
+                    length = lps[length - 1]
+
+                else:
+                    lps[i] = 0
+                    i += 1
+
+        i = 0
+        j = 0
+
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+
+                if j == len(needle):
+                    return i - j
+  
+            else:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+
+        return -1
